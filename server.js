4546950b37017f7d37c9cf9513bbca9df9ab6938
DeepSheth2003 <deepsheth56@gmail.com>
2026-02-26 00:26:56 +0530

    prepare backend for render

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
